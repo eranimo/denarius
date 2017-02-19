fd564fdcc7aefd0e96b9324f9b82eab74dc2b1bd
Kaelan Cooter <dcgamers@gmail.com>
2017-02-19 17:23:17 -0500

    initial commit: project set up

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// @flow
+class Job {
+  constructor() {
+
+  }
+}
+
+class Trader {
+  constructor(job: Job) {
+
+  }
+}
+
+class Market {
+  constructor() {
+
+  }
+
+  // trades between traders at this market
+  simulate() {
+
+  }
+}
+
+function start() {
+  const market: Market = new Market();
+}
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = function(env) {
+  return {
+    entry: './src/index.js',
+    devtool: "cheap-eval-source-map",
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        }
+      ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+      title: 'Denarius - Economic Simulation'
+    })],
+    devServer: {
+      stats: 'errors-only'
+    }
+  }
+}
